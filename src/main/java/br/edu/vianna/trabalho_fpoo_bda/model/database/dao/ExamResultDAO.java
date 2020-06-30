b8c17f15f586da5777d0f8d3570f1d7d76475018
NathanOliveiraJF <nathanoliveira003jf@gmail.com>
2020-06-29 22:00:05 -0300

    ExamResultDao implementada

---
+++ src/main/java/br/edu/vianna/trabalho_fpoo_bda/model/database/dao/ExamResultDAO.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.vianna.trabalho_fpoo_bda.model.database.dao;
+
+import br.edu.vianna.trabalho_fpoo_bda.exception.NotConnectionException;
+import br.edu.vianna.trabalho_fpoo_bda.model.ExamResult;
+import br.edu.vianna.trabalho_fpoo_bda.model.database.connection.ConnectionSingleton;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author natha
+ */
+public class ExamResultDAO implements IGenericsDAO<ExamResult, Integer> {
+
+    @Override
+    public void inserir(ExamResult obj) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void alterar(ExamResult obj) throws NotConnectionException, SQLException {
+        Connection c = ConnectionSingleton.getConnection();
+
+        String sql = "UPDATE ResultadoExame "
+                + "SET  "
+                + "id = ?  "
+                + "descricao = ? "
+                + "WHERE id  = ?";
+
+        PreparedStatement st = c.prepareStatement(sql);
+        
+        st.setInt(1, obj.getIdResultadoExame());
+        st.setString(2, obj.getDescricao());
+        
+        st.executeUpdate();
+    }
+
+    @Override
+    public void apagar(ExamResult obj) throws NotConnectionException, SQLException {
+        Connection c = ConnectionSingleton.getConnection();
+
+        String sql = "DELETE FROM ResultadoExame "
+                + "WHERE id = ?";
+
+        PreparedStatement st = c.prepareStatement(sql);
+
+        st.setInt(1, obj.getIdResultadoExame());
+
+        st.executeUpdate();
+    }
+
+    @Override
+    public ExamResult buscarPeloId(Integer key) throws NotConnectionException, SQLException {
+        Connection c = ConnectionSingleton.getConnection();
+
+        String sql = "SELECT * FROM ResultadoExame as res "
+                + "INNER JOIN Exame as ex ON (res.id = ex.idResultadoExame) "
+                + "WHERE res.id = ?";
+
+        PreparedStatement st = c.prepareStatement(sql);
+
+        st.setInt(1, key);
+
+        ResultSet rs = st.executeQuery();
+
+        ExamResult ex = null;
+        if (rs.next()) {
+            ex = new ExamResult(rs.getInt("id"),
+                    rs.getString("descricao"));
+
+            return ex;
+        } else {
+            return ex;
+        }
+    }
+
+    @Override
+    public int quantidade() throws NotConnectionException, SQLException {
+        Connection c = ConnectionSingleton.getConnection();
+
+        String sql = "SELECT count(*) FROM ResultadoExame res"
+                + "INNER JOIN Exame as ex ON (res.id = ex.idResultadoExame)";
+
+        PreparedStatement st = c.prepareStatement(sql);
+
+        ResultSet rs = st.executeQuery();
+
+        rs.next();
+
+        return rs.getInt(1);
+    }
+
+}
